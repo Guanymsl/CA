a48cfdcc391730ae842091dc7339da436f8b0de7
Guanymsl <chadlee9402@gmail.com>
2024-11-27 16:05:09 +0800

    Fix MUL and DIV

--- hw2/ALU.v
+++ hw2/ALU.v
@@ -13,9 +13,10 @@ module ALU (
 //                    Registers
 // ===============================================
 reg [63:0] product;
-reg [31:0] dividend, divisor;
-reg [31:0] quotient, remainder;
-reg [5:0]  cycle_count;
+reg [63:0] remainder;
+reg [31:0] divisor;
+reg [31:0] multiplicand;
+reg [5:0]  count;
 reg mul_active, div_active;
 
 // ===============================================
@@ -24,7 +25,7 @@ reg mul_active, div_active;
 always @(*) begin
     ready = 1'b0;
     out_data = 64'd0;
-    if (!mul_active && !div_active && valid) begin
+    if (valid) begin
         case (mode)
             4'b0000: out_data = {32'd0, in_A + in_B};
             4'b0001: out_data = {32'd0, in_A - in_B};
@@ -36,7 +37,8 @@ always @(*) begin
             4'b0111: out_data = {32'd0, in_A >> in_B};
             4'b1000: out_data = {32'd0, in_A << in_B};
         endcase
-        ready = 1'b1;
+        if (mode >= 4'b0000 && mode <= 4'b1000)
+            ready = 1'b1;
     end
 end
 
@@ -48,48 +50,48 @@ always @(posedge clk or negedge rst_n) begin
         ready <= 1'b0;
         mul_active <= 1'b0;
         div_active <= 1'b0;
-        cycle_count <= 6'd0;
-        product <= 64'd0;
-        quotient <= 32'd0;
-        remainder <= 32'd0;
         out_data <= 64'd0;
+        count <= 6'd0;
     end else if (valid) begin
         case (mode)
             4'b1001: begin
                 mul_active <= 1'b1;
-                product <= in_A * in_B;
-                cycle_count <= 6'd1;
-                ready <= 1'b0;
+                count <= 6'd0;
+                multiplicand <= in_A;
+                product <= {32'd0, in_B};
             end
             4'b1010: begin
                 div_active <= 1'b1;
-                dividend <= in_A;
+                count <= 6'd0;
                 divisor <= in_B;
-                quotient <= 32'd0;
-                remainder <= 32'd0;
-                cycle_count <= 6'd32;
-                ready <= 1'b0;
+                remainder <= {32'd0, in_A} << 1;
             end
         endcase
     end else if (mul_active) begin
-        if (cycle_count > 0) begin
-            cycle_count <= cycle_count - 1;
+        if (count < 32) begin
+            if (product[0] == 1'b1) begin
+                product <= product + {multiplicand, 32'd0};
+            end
+            product <= product >> 1;
+            count <= count + 1;
         end else begin
             mul_active <= 1'b0;
-            ready <= 1'b1;
             out_data <= product;
+            ready <= 1'b1;
         end
     end else if (div_active) begin
-        if (cycle_count > 0) begin
-            cycle_count <= cycle_count - 1;
-            if (dividend >= divisor) begin
-                dividend <= dividend - divisor;
-                quotient <= quotient + 1;
+        if (cycle_count < 32) begin
+            remainder = remainder - {divisor, 32'd0}
+            if (remainder[63] == 1'b1) begin
+                remainder = remainder + {divisor, 32'd0} << 1
+            end else begin
+                remainder = {remainder, 1'b1} << 1
             end
+            count <= count + 1;
         end else begin
             div_active <= 1'b0;
+            out_data <= remainder >> 1;
             ready <= 1'b1;
-            out_data <= {dividend, quotient};
         end
     end
 end
